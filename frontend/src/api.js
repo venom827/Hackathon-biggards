cac932e6cc5bc1391e10a160f0a310781e5fc735
venom827 <gvenom680@gmail.com>
2025-11-05 06:08:55 +0000

    Added the backend link to connect frontend and backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hackathon-biggards-production-1df7.up.railway.app/",
 });
 
 API.interceptors.request.use((req) => {
