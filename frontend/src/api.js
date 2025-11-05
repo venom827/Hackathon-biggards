d410e12d855c5650e8cbfd447713995d8b0d73b1
venom827 <gvenom680@gmail.com>
2025-11-05 06:16:42 +0000

    Fixed issues with frontend backend connection

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hackathon-biggards-production-1df7.up.railway.app/",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 API.interceptors.request.use((req) => {
